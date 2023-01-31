b8f3c12f6cd5953faf8412aa8e9778dd983250cd
Victor Clivatti <joaoclivatti@hotmail.com>
2023-01-31 14:03:44 -0300

    3rd Commit

---
+++ teste.js
@@ -0,0 +1,5 @@
+function processInput() {
+  var input = document.getElementById("inputField").value
+  var output = "Irei te contatar pelo email: " + input + ", muito obrigado."
+  document.getElementById("outputField").innerHTML = output
+}
